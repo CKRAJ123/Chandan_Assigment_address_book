460d5e88437e8c2a47bf3b73a0f85c55a46d6c25
CKRAJ123 <91893156+CKRAJ123@users.noreply.github.com>
2023-01-28 17:59:27 +0530

    Update Chandan.java

--- Chandan.java
+++ Chandan.java
@@ -1,4 +1,3 @@
-/* package codechef; // don't place package name! */
 
 import java.util.*;
 import java.lang.*;
@@ -7,47 +6,91 @@ import java.io.*;
 /* Name of the class has to be "Main" only if the class is public. */
 class Address
 {
+	JSONArray addressBook=[{
+                                fName : 'John',
+                                lName : 'Smith',
+				address : '123 street,xyz',
+		                city : 'Patna'
+                                state : 'Abc',
+                                postCode : '823443'
+		              },
+			       {
+                                fName : 'Darmendra',
+                                lName : 'Singh',
+				address : 'Kanchanpur',
+				city : 'Gaya',      
+                                state : 'Bihar',
+                                postCode : '123443'
+		              },
+			       {
+                                fName: 'Chandan',
+                                lName: 'Kumar',
+				address: 'Kanchanpur',
+				city : 'Gaya',     
+                                state: 'Bihar',
+                                postCode: '123443'
+		              },
+			       {
+                                fName: 'Tommy',
+                                lName: 'Singh',
+				address: '123 Main St.',
+				city : 'Gaya',
+                                state: 'Seattle',
+                                postCode: '43'
+		              },
+			       fName : 'Alice',
+                                lName : 'Smith',
+				address : '123 main st.',
+		                city : 'seattle'
+                                state : 'wa',
+                                postCode : '45'
+		              },
+			       {
+                                fName : 'bOb',
+                                lName : 'Williams',
+				address : '234 2nd Ave.',
+				city : 'Tacoma',      
+                                state : 'WA',
+                                postCode : '26'
+		              },
+			       {
+                                fName: 'Carol',
+                                lName: 'Johnson',
+				address: '234 2nd Ave',
+				city : 'Seattle',     
+                                state: 'WA',
+                                postCode: '67'
+		              },
+			       {
+                                fName: 'TOm'
+                                lName: 'Bombadillo',
+				address: '1212 Maple Street',
+				city : 'Florida',
+                                state: 'GA',
+                                postCode: '49'
+		              }
+                      ]
 	public static void main (String[] args) throws java.lang.Exception
 	{
-	     	String[] addressBook = {"Dave","Smith","123 main st.","seattle","wa","43",
-                                    "Alice","Smith","123 Main St.","Seattle","WA","45",
-                                    "bOb","Williams","234 2nd Ave.","Tacoma","WA","26",
-                                    "Carol","Johnson","234 2nd Ave","Seattle","WA","67",
-                                    "TOm","Bombadillo","1212 Maple Street","Florida","GA","520",
-                                    "Jimbo","Jones","82 Pine Street","Atlanta","GA","2",
-                                    "Jackie","Jones","82 Pine Street","Atlanta","GA","6",
-                                    "Tommy","Jones","82 Pine Street","Atlanta","GA","29",
-                                    "tammy","Jones","82 Pine Street","Atlanta","GA","27",
-                                    "EvE","Smith","234 2nd Ave.","Tacoma","WA","25",
-                                    "Frank","Jones","234 2nd Ave.","Tacoma","FL","23",
-                                    "george","Brown","345 3rd Blvd., Apt. 200","Seattle","WA","19",
-                                    "Helen","Brown","345 3rd Blvd. Apt. 200","Seattle","WA","18",
-                                    "Ian","smith","123 main st ","Seattle","Wa","18",
-                                     "Jane","Smith","123 Main St.","Seattle","WA","13"};
-    
-	       System.out.println(search(addressBook));
-
+		Scanner sc = new Scanner(System.in); 
+		String contact = sc.nextLine();
+	     
+	       System.out.println(search(contact));
 	}
 	
 	
-	 public static String search(String[] addressBook) { 
-        
-                    Map<String,Integer> mp=new HashMap<>();
-        
+	 public static String search(String contact) { 
+       
                    for(int i=0;i<addressBook.length;i++){
-                     if (mp.containsKey(addressBook[i]))
-                        {
-                          return addressBook[i];
-                         // mp.put(arr[i], mp.get(arr[i]) + 1);
-                       }
-                     else
-                        {
-                         mp.put(addressBook[i], 1);
-                     }
-              }
-            return "no contact matched from address book";
-        
-    }
+                      if(addressBook[i]['fName']==contact || addressBook[i]['lName']==contact ||
+			 addressBook[i]['address']==contact || addressBook[i]['city']==contact|| 
+			 addressBook[i]['state']==contact || addressBook[i]['postCode']==contact ){
+			     return addressBook[i];
+		      }
+                   }
+		 
+            return "no contact data matched from address book";  
+       }
 }
 
 
